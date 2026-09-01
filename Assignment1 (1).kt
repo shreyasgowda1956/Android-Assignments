845a2a60b428176fdc2efd33e2ea6b1a0a4181e9
shreyasgowda1956 <shreyasg1956@gmail.com>
2026-09-01 08:57:00 +0530

    Add files via upload

---
+++ Assignment1 (1).kt
@@ -0,0 +1,12 @@
+package com.pes.lib
+
+fun main() {
+
+    val data = "Hi! How are you?"
+
+    // 1. how many vowels in the string
+
+    // 2. how many spaces
+
+    // use of 'when' as a expression
+}
